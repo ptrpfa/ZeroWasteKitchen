820a1e440b5edab02f5dc2c62cc262b1817ab4cd
JRRZZ <Jianrong12@outlook.com>
2023-06-14 16:57:46 +0800

    test

--- web/argon/apps/home/views.py
+++ web/argon/apps/home/views.py
@@ -5,7 +5,7 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from django.urls import reverse
-from django.db import connection
+from django.db import connection, connections
 from django.shortcuts import render
 from core import settings
 
