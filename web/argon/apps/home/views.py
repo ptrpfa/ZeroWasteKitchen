3c05214a6fda4a3a1994cb2b21bb11256fd65a38
JRRZZ <Jianrong12@outlook.com>
2023-06-14 20:39:51 +0800

    edited some stuff

--- web/argon/apps/home/views.py
+++ web/argon/apps/home/views.py
@@ -445,7 +445,7 @@ def view_recipe(request, id):
             'Carbohydrates': check_field(nutrition_data.get('Carbohydrates')),
             'Protein': check_field(nutrition_data.get('Protein')),
             'Reviews': reviews_data.get('Reviews', []) if reviews_data else [],
-            'OverallRating': check_field(reviews_data.get('OverallRating')) if reviews_data else 'Not Available'
+            'OverallRating': check_field(reviews_data.get('Overall_Rating')) if reviews_data else 'Not Available'
         }
     }
 
